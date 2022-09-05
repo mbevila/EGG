a08721fdbc55fbd4c8d7bc4fc3132597afe9fa86
robertodessi <roberto.dessi11@gmail.com>
2022-09-05 16:31:08 -0700

    fix eos stopping and sampling/beam search

--- egg/zoo/emergent_captioner/finetuning/sender.py
+++ egg/zoo/emergent_captioner/finetuning/sender.py
@@ -3,17 +3,44 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from types import MethodType
 from typing import Any, Dict, Tuple
 
 import clip
 import torch
 import torch.nn as nn
 from torch.distributions import Categorical
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-from undecorated import undecorated
+from transformers import (
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+    LogitsProcessor,
+    LogitsProcessorList,
+)
 
 from egg.zoo.emergent_captioner.utils import convert_models_to_fp32
+
+
+class StopTokenLogitsProcessor(LogitsProcessor):
+    def __init__(self, tokenizer, do_sample):
+        self.eos_token_id = tokenizer.eos_token_id
+
+        self.stop_word_ids = set(
+            [
+                idx
+                for idx in range(len(tokenizer))
+                if "." in tokenizer.convert_ids_to_tokens(idx)
+            ]
+        )
+        self.vocab_size = len(tokenizer)
+
+    def __call__(
+        self, input_ids: torch.LongTensor, scores: torch.FloatTensor
+    ) -> torch.FloatTensor:
+        for i, input_id in enumerate(input_ids):
+            if input_id[-1].item() in self.stop_word_ids:
+                scores[i, : self.vocab_size] = torch.finfo().min
+                scores[i, self.vocab_size :] = float("-inf")
+                scores[i, self.eos_token_id] = 0.0
+        return scores
 
 
 class MLP(nn.Module):
@@ -34,145 +61,140 @@ class ClipCapModel(nn.Module):
     def __init__(
         self,
         clip_prefix_size: int,
-        clip_prefix_tokens: int,
+        nb_prefix_tokens: int,
+        num_return_sequences: int = 1,
+        do_sample: bool = False,
         beam_size: int = 5,
         max_len: int = 20,
     ):
         super(ClipCapModel, self).__init__()
 
-        assert beam_size > 1
-
-        self.clip_prefix_tokens = clip_prefix_tokens
+        self.nb_prefix_tokens = nb_prefix_tokens
 
         self.gpt = GPT2LMHeadModel.from_pretrained("gpt2")
-        self.gpt_embedding_size = self.gpt.transformer.wte.weight.shape[1]
-        self.gpt.config.pad_token_id = self.gpt.config.eos_token_id
-
-        generate_with_grad = undecorated(self.gpt.generate)
-        self.gpt.generate_with_grad = MethodType(generate_with_grad, self.gpt)
-
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-        self.tokenizer.pad_token = self.gpt.config.pad_token_id
+        self.gpt.config.pad_token_id = self.gpt.config.eos_token_id
+        self.eos_token_id = self.tokenizer.eos_token_id
 
+        self.num_return_sequences = num_return_sequences
+        self.do_sample = do_sample
         self.beam_size = beam_size
         self.max_len = max_len
 
-        if clip_prefix_tokens > 10:  # not enough memory
+        self.logits_processor = StopTokenLogitsProcessor(self.tokenizer, do_sample)
+
+        gpt_embedding_size = self.gpt.transformer.wte.weight.shape[1]
+        if nb_prefix_tokens > 10:  # not enough memory
             input_dim = clip_prefix_size
-            output_dim = self.gpt_embedding_size * clip_prefix_tokens
+            output_dim = gpt_embedding_size * nb_prefix_tokens
             self.clip_project = nn.Linear(input_dim, output_dim)
         else:
             input_dim = clip_prefix_size
-            hidden_dim = (self.gpt_embedding_size * clip_prefix_tokens) // 2
-            output_dim = self.gpt_embedding_size * clip_prefix_tokens
+            hidden_dim = (gpt_embedding_size * nb_prefix_tokens) // 2
+            output_dim = gpt_embedding_size * nb_prefix_tokens
             self.clip_project = MLP((input_dim, hidden_dim, output_dim))
 
-    def setup_input_output_embeddings(self, bsz, prefix_len):
-        self.gpt.resize_token_embeddings(len(self.tokenizer) + (prefix_len * bsz))
+    def maybe_patch_gpt(self, max_embeddings):
+        if not getattr(self.gpt, "_patched", False):
+            self.gpt._patched = True
+            self.gpt.resize_token_embeddings(len(self.tokenizer) + max_embeddings)
+            if self.gpt.get_output_embeddings().bias is None:
+                self.gpt.get_output_embeddings().bias = torch.nn.Parameter(
+                    torch.tensor([0.0] * (len(self.tokenizer) + max_embeddings))
+                )
+                self.gpt.get_output_embeddings().bias.requires_grad = False
+                self.gpt._originally_with_no_bias = True
+            else:
+                self.gpt._originally_with_no_bias = False
+            self.gpt.get_output_embeddings().bias.data[-max_embeddings:] = float("-inf")
 
-    def get_greedy_baseline(self):
-        generated_ids = self.gpt.generate(
-            self.cached_input_ids, max_length=self.max_len + self.clip_prefix_tokens
-        )
-
-        decoded = self.tokenizer.batch_decode(
-            generated_ids[:, self.clip_prefix_tokens :],
-            skip_special_tokens=True,
-            clean_up_tokenization_spaces=True,
-        )
-
-        captions = []
-        for caption in decoded:
-            text = caption + "."
-            filtered = text[: text.index(".") + 1]
-            captions.append(filtered)
-
-        return captions
+    def maybe_unpatch_gpt(self):
+        if getattr(self.gpt, "_patched", False):
+            self.gpt._patched = False
+            self.gpt.resize_token_embeddings(len(self.tokenizer))
+            if self.gpt._originally_with_no_bias:
+                self.gpt.get_output_embeddings().bias = None
 
     def forward(self, image_feats, aux_input=None):
-        bsz = image_feats.shape[0]
+        prompts = self.clip_project(image_feats)
+        prompts = prompts.view(image_feats.shape[0], self.nb_prefix_tokens, -1)
 
-        prefix_embed = self.clip_project(image_feats)
+        bsz, prefix_len, h_dim = prompts.shape
 
-        prefix_embed = prefix_embed.view(bsz, self.clip_prefix_tokens, -1)
-        bsz, prefix_len, h_dim = prefix_embed.shape
+        prompts_flat = prompts.view(-1, prompts.size(-1))
+        start = len(self.tokenizer)
+        end = start + (bsz * prefix_len)
+        self.gpt.get_input_embeddings().weight.data[start:end] = prompts_flat
+        input_ids = torch.arange(start, end).view(*prompts.shape[:2]).to(prompts.device)
 
-        bias = torch.Tensor(
-            [0.0] * len(self.tokenizer) + [float("-inf")] * bsz * prefix_len
-        ).to(prefix_embed.device)
-        self.gpt.get_output_embeddings().bias = nn.Parameter(bias, requires_grad=False)
-
-        prefix_embed_flat = prefix_embed.view(-1, h_dim)
-        start, end = len(self.tokenizer), len(self.tokenizer) + (bsz * prefix_len)
-        self.gpt.get_input_embeddings().weight.data[start:end] = prefix_embed_flat
-        input_ids = torch.arange(start, end)
-        input_ids = input_ids.view(bsz, prefix_len).to(prefix_embed.device)
-        self.cached_input_ids = input_ids
-
-        generated = self.gpt.generate_with_grad(
+        generated = self.gpt.generate(
             input_ids,
-            max_length=self.max_len + self.clip_prefix_tokens,
-            output_scores=True,
-            return_dict_in_generate=True,
+            do_sample=self.do_sample,
+            max_length=self.max_len,
+            num_beams=self.beam_size,
+            num_return_sequences=self.num_return_sequences,
+            logits_processor=LogitsProcessorList([self.logits_processor]),
+            top_k=len(self.tokenizer),
         )
+        if self.num_return_sequences > 1:
+            raise NotImplementedError
+            # needs to handle multiple sequences on the receiver side as well
+            # prompts = prompts.repeat_interleave(self.num_return_sequences, 0)
 
-        max_k = generated.sequences.size(1) - prefix_len
-        zero_mask = generated.sequences[:, prefix_len:] == self.tokenizer.encode(".")[0]
-        zero_mask = zero_mask.cumsum(dim=1) > 0
-        msg_lengths = max_k - (zero_mask).sum(dim=1)
+        indices = generated[:, prefix_len:]
+        suffix = self.gpt.get_input_embeddings()(indices)
+        inputs_embeds = torch.cat([prompts, suffix], dim=1)
+        logits = self.gpt(inputs_embeds=inputs_embeds)
+        logits = logits[0][:, prefix_len - 1 : -1, : len(self.tokenizer)]
+        logits = logits.log_softmax(-1)
+
+        # compute_mask and msg_lengths
+        max_k = indices.size(1)
+        end_of_caption = indices == self.eos_token_id
+        extra_tokens = end_of_caption.cumsum(dim=1) > 0
+        msg_lengths = max_k - (extra_tokens).sum(dim=1)
         msg_lengths.add_(1).clamp_(max=max_k)
 
-        log_probs = generated.scores
-        log_probs = torch.stack(log_probs, dim=1)[..., : len(self.tokenizer)]
+        mask = (extra_tokens == 0).float()
 
-        entropy = torch.stack(
-            [
-                Categorical(probs=log_probs[:, timestep].detach().softmax(-1)).entropy()
-                for timestep in range(log_probs.shape[1])
-            ],
-            dim=1,
-        )
+        # compute normalized log_probs of generated captions
+        log_probs = torch.gather(logits, dim=2, index=indices.unsqueeze(1)).squeeze()
+        log_probs *= mask
+        log_probs = log_probs.sum(1) / msg_lengths
 
-        entropy *= (~zero_mask).long()
+        # compute avg per timestep entropy of captioner distribution
+        entropies = []
+        for timestep in range(logits.shape[1]):
+            dist = Categorical(probs=logits[:, timestep].detach())
+            ent = dist.entropy() * mask[:, timestep]
+            entropies.append(ent)
+
+        entropy = torch.stack(entropies, dim=1)
         entropy = entropy.sum(-1) / msg_lengths
 
-        log_probs = torch.topk(log_probs, largest=True, k=1, dim=-1)[0].squeeze()
-        log_probs *= (~zero_mask).long()
-        log_probs = log_probs.sum(-1) / msg_lengths
-
-        decoded = self.tokenizer.batch_decode(
-            generated.sequences[:, self.clip_prefix_tokens :],
+        # put captions in textual form
+        decoded_captions = self.tokenizer.batch_decode(
+            indices,
             skip_special_tokens=True,
             clean_up_tokenization_spaces=True,
         )
-
-        captions = []
-        for caption in decoded:
-            text = caption + "."
-            filtered = text[: text.index(".") + 1]
-            captions.append(filtered)
-
-        return captions, log_probs, entropy, msg_lengths
-
-    def named_parameters(self, prefix="", recurse: bool = True):
-        return self.gpt.named_parameters()
-
-    def parameters(self, recurse: bool = True):
-        return self.gpt.parameters()
+        return decoded_captions, log_probs, entropy, msg_lengths
 
 
 class ClipCapSender(nn.Module):
     def __init__(
         self,
-        clip_prefix_tokens: int,
+        nb_prefix_tokens: int,
         clip_model: str,
         clipcap_path: str,
+        num_return_sequences: int = 1,
+        do_sample: bool = False,
         beam_size: int = 5,
         max_len: int = 20,
     ):
         super(ClipCapSender, self).__init__()
 
-        assert max_len < 75  # Clip maximum context size
+        assert max_len < 75  # clip maximum context size
 
         self.clip_vit = clip.load(clip_model)[0].visual
         convert_models_to_fp32(self.clip_vit)
@@ -183,26 +205,17 @@ class ClipCapSender(nn.Module):
 
         self.clipcap = ClipCapModel(
             clip_prefix_size=self.clip_vit.output_dim,
-            clip_prefix_tokens=clip_prefix_tokens,
+            nb_prefix_tokens=nb_prefix_tokens,
+            num_return_sequences=num_return_sequences,
+            do_sample=do_sample,
             beam_size=beam_size,
             max_len=max_len,
         )
         if clipcap_path is not None:
             self.clipcap.load_state_dict(torch.load(clipcap_path))
 
-    def setup_clipcap(self, batch_size, n_prefix_tokens):
-        self.clipcap.setup_input_output_embeddings(batch_size, n_prefix_tokens)
-
     def encode_images(self, images: torch.Tensor):
         return self.clip_vit(images)
-
-    def get_greedy_baseline(self):
-        return self.clipcap.get_greedy_baseline()
-
-    def forward(self, images: torch.Tensor, aux_input: Dict[Any, torch.Tensor] = None):
-        image_feats = self.encode_images(images)
-        captions, log_probs, entropy, msg_lengths = self.clipcap(image_feats, aux_input)
-        return captions, log_probs, entropy, msg_lengths
 
     def named_parameters(self, prefix="", recurse: bool = True):
         return self.clipcap.named_parameters()
@@ -215,3 +228,11 @@ class ClipCapSender(nn.Module):
         self.clip_vit.eval()
         self.clipcap.train(mode)
         return self
+
+    def patch_model(self, batch_size: int, nb_prefix_tokens: int):
+        self.clipcap.maybe_patch_gpt(batch_size * nb_prefix_tokens)
+
+    def forward(self, images: torch.Tensor, aux_input: Dict[Any, torch.Tensor] = None):
+        image_feats = self.encode_images(images)
+        captions, log_probs, entropy, msg_lengths = self.clipcap(image_feats, aux_input)
+        return captions, log_probs, entropy, msg_lengths
