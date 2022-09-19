9530180884d2ae9faca33a7786e5db4b38d40e61
robertodessi <roberto.dessi11@gmail.com>
2022-09-19 10:02:57 -0700

    conceptual dataloader first version

--- egg/zoo/emergent_captioner/dataloaders/conceptual_captions_dataloader.py
+++ egg/zoo/emergent_captioner/dataloaders/conceptual_captions_dataloader.py
@@ -5,14 +5,16 @@
 
 import csv
 import os
-import random
+
+# import random
 import zlib
 from pathlib import Path
 from typing import Callable, Optional
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 
 import torch
 import torch.distributed as dist
+from timebudget import timebudget
 from torchvision.datasets import VisionDataset
 
 from egg.zoo.emergent_captioner.dataloaders.utils import (
@@ -24,10 +26,12 @@ from egg.zoo.emergent_captioner.dataloaders.utils import (
 class ConceptualCaptionsDataset(VisionDataset):
     def __init__(
         self,
-        dataset_dir: str = "/private/home/rdessi/ConceptualCaptions",
+        dataset_dir: str = None,
         split: str = "train",
         transform: Optional[Callable] = None,
     ):
+        if dataset_dir is None:
+            dataset_dir = "/private/home/rdessi/ConceptualCaptions"
         super(ConceptualCaptionsDataset, self).__init__(
             dataset_dir, transform=transform
         )
@@ -35,22 +39,29 @@ class ConceptualCaptionsDataset(VisionDataset):
         assert split in ["train", "test"]
 
         if split == "train":
-            annotations_file = "Train_GCC-training.tsv"
+            annotations_file = "train_conceptual_captions_paths.txt"
             self.image_folder = self.dataset_dir / "training"
         else:
+            # annotations_file = "train_conceptual_captions_paths.txt"
             annotations_file = "Validation_GCC-1.1.0-Validation.tsv"
             self.image_folder = self.dataset_dir / "validation"
 
-        all_images = {f for f in os.listdir(self.image_folder) if f[0].isdigit()}
+        with timebudget("0"):
+            all_images = {
+                f.name for f in os.scandir(self.image_folder) if f.name[0].isdigit()
+            }
 
-        self.samples = []
-        with open(self.dataset_dir / annotations_file) as fd:
-            reader = csv.reader(fd, delimiter="\t")
-            for i, (caption, url) in enumerate(reader):
-                filename = f"{i}_{zlib.crc32(url.encode('utf-8')) & 0xFFFFFFFF}"
-                if filename in all_images:
-                    self.samples.append((caption, filename))
-        self.idx = -1
+        with timebudget("1"):
+            self.samples = []
+            with open(self.dataset_dir / annotations_file) as fd:
+                reader = csv.reader(fd, delimiter="\t")
+                for i, (caption, url) in enumerate(reader):
+                    filename = f"{i}_{zlib.crc32(url.encode('utf-8')) & 0xFFFFFFFF}"
+                    if filename in all_images:
+                        self.samples.append((caption, filename))
+        with open("/private/home/rdessi/test_conceptual_captions_paths.txt", "w") as f:
+            for sample in self.samples:
+                f.write(f"{sample[1].strip()}\n")
 
     def __len__(self):
         return len(self.samples)
@@ -60,20 +71,13 @@ class ConceptualCaptionsDataset(VisionDataset):
 
         img_path = self.image_folder / fname
 
-        try:
-            with open(img_path, "rb") as f:
-                image = Image.open(f).convert("RGB")
-        except UnidentifiedImageError:
-            return self.__getitem__(random.choice(self.samples))
+        with open(img_path, "rb") as f:
+            image = Image.open(f).convert("RGB")
 
         if self.transform:
             image = self.transform(image)
 
-        aux = {
-            "img_id": fname,
-            "caption": caption,
-            "all_captions": caption,
-        }
+        aux = {"img_id": torch.tensor([fname])}
         return image, torch.tensor([index]), image, aux
 
 
@@ -105,7 +109,7 @@ class ConceptualCaptionsWrapper:
         loader = torch.utils.data.DataLoader(
             ds,
             batch_size=batch_size,
-            shuffle=split != "test" and sampler is None,
+            shuffle=False,  # split != "test" and sampler is None,
             sampler=sampler,
             num_workers=num_workers,
             pin_memory=True,
@@ -120,12 +124,10 @@ if __name__ == "__main__":
     dl = wrapper.get_split(
         split="test",
         batch_size=1,
-        image_size=224,
+        image_size=32,
         num_workers=8,
     )
-
-    for i, (_, idx, _, aux) in enumerate(dl):
-        if i == 5000:
-            break
-        if i % 1000 == 0:
+    print("len data", len(dl.dataset))
+    with timebudget("all"):
+        for i, elem in enumerate(dl):
             print("eleem", i)
